0e8d941878277ac3b232422008589c1a998a047e
edward vega <edward.vega@wiedii.co>
2024-05-14 20:11:02 -0500

    add ng zorro and layout style header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
+export interface AutocompleteOptionGroups {
+  title: string;
+  count?: number;
+  children?: AutocompleteOptionGroups[];
+}
 
 @Component({
   selector: 'app-header',
+  encapsulation: ViewEncapsulation.None,
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NzButtonModule,
+    NzAutocompleteModule,
+    NzIconModule,
+    NzInputModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  inputValue?: string;
+  optionGroups: AutocompleteOptionGroups[] = [];
 
+  constructor() {}
+
+  onChange(value: string): void {
+    console.log(value);
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.optionGroups = [
+        {
+          title: '话题',
+          children: [
+            {
+              title: 'AntDesign',
+              count: 10000
+            },
+            {
+              title: 'AntDesign UI',
+              count: 10600
+            }
+          ]
+        },
+        {
+          title: '问题',
+          children: [
+            {
+              title: 'AntDesign UI 有多好',
+              count: 60100
+            },
+            {
+              title: 'AntDesign 是啥',
+              count: 30010
+            }
+          ]
+        },
+        {
+          title: '文章',
+          children: [
+            {
+              title: 'AntDesign 是一个设计语言',
+              count: 100000
+            }
+          ]
+        }
+      ];
+    }, 1000);
+  }
 }
