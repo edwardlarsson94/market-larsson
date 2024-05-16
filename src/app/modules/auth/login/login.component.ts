1ea88cae67ca69a0200b98f686c824c5e383c684
edward vega <edward.vega@wiedii.co>
2024-05-15 23:05:56 -0500

    0009 add layout login

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { FormControl, FormGroup, NonNullableFormBuilder, Validators,  ReactiveFormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     NzModalModule,
-    NzButtonModule
+    NzButtonModule,
+    NzFormModule,
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   isVisible = false;
-
-  constructor() {}
 
   showModal(): void {
     this.isVisible = true;
@@ -30,4 +32,30 @@ export class LoginComponent {
     console.log('Button cancel clicked!');
     this.isVisible = false;
   }
+
+  validateForm: FormGroup<{
+    userName: FormControl<string>;
+    password: FormControl<string>;
+    remember: FormControl<boolean>;
+  }> = this.fb.group({
+    userName: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    remember: [true]
+  });
+
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log('submit', this.validateForm.value);
+    } else {
+      Object.values(this.validateForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+
+  constructor(private fb: NonNullableFormBuilder) {}
+
 }
