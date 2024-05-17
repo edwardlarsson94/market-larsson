5fc852eb81d6db696ff3d63e02aa9b4447100b2b
edward vega <edward.vega@wiedii.co>
2024-05-16 21:12:18 -0500

    0012 add styles cards of cart shopping

--- src/app/modules/cart/cards/cards.component.ts
+++ src/app/modules/cart/cards/cards.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMinus, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+
 
 @Component({
   selector: 'app-cards',
   standalone: true,
-  imports: [],
+  imports: [
+    NzCardModule,
+    FontAwesomeModule,
+    NzButtonModule
+  ],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.css'
 })
 export class CardsComponent {
-
+  faTrash = faTrash
+  faPlus = faPlus
+  faMinus = faMinus
 }
