64fbe900edd4a34de1baf9d322cd6cd0d6012fc4
edward vega <edward.vega@wiedii.co>
2024-05-16 22:28:00 -0500

    0015 refactor second styles card products

--- src/app/modules/catalog/cards/cards.component.ts
+++ src/app/modules/catalog/cards/cards.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzButtonModule,NzButtonSize } from 'ng-zorro-antd/button';
+import { faMinus, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-cards',
   standalone: true,
   imports: [
     NzCardModule,
     NzButtonModule,
+    FontAwesomeModule
   ],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.css'
 })
 export class CardsComponent {
   size: NzButtonSize = 'large';
+  faTrash = faTrash
+  faPlus = faPlus
+  faMinus = faMinus
 }
