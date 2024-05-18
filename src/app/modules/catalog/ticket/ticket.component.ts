a0e3bf1d371df5c91c312d0460702681bd1908bd
edward vega <edward.vega@wiedii.co>
2024-05-18 14:07:10 -0500

    0022 layout and styles tickets also styles refactor login and register

--- src/app/modules/catalog/ticket/ticket.component.ts
+++ src/app/modules/catalog/ticket/ticket.component.ts
@@ -2,17 +2,68 @@ import { Component } from '@angular/core';
 import { BannerComponent } from '../../../shared/banner/banner.component';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  ValidationErrors,
+  Validators,
+  ReactiveFormsModule
+} from '@angular/forms';
+import { Observable, Observer } from 'rxjs';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+
 @Component({
   selector: 'app-ticket',
   standalone: true,
   imports: [
     BannerComponent,
     NzGridModule,
-    NzButtonModule
+    NzButtonModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzInputModule
   ],
   templateUrl: './ticket.component.html',
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
+  validateForm: FormGroup<{
+    address: FormControl<string>;
+    email: FormControl<string>;
+    comment: FormControl<string>;
+  }>;
 
+  submitForm(): void {
+    console.log('submit', this.validateForm.value);
+  }
+
+  resetForm(e: MouseEvent): void {
+    e.preventDefault();
+    this.validateForm.reset();
+  }
+
+  userNameAsyncValidator: AsyncValidatorFn = (control: AbstractControl) =>
+    new Observable((observer: Observer<ValidationErrors | null>) => {
+      setTimeout(() => {
+        if (control.value === 'Colombia') {
+          // you have to return `{error: true}` to mark it as an error event
+          observer.next({ error: true, duplicated: true });
+        } else {
+          observer.next(null);
+        }
+        observer.complete();
+      }, 1000);
+    });
+
+  constructor(private fb: NonNullableFormBuilder) {
+    this.validateForm = this.fb.group({
+      address: ['', [Validators.required], [this.userNameAsyncValidator]],
+      email: ['', [Validators.email, Validators.required]],
+      comment: ['', [Validators.required]]
+    });
+  }
 }
