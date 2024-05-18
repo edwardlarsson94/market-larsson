0b85d5487575bea6fe02793cf9ee0544df383acf
edward vega <edward.vega@wiedii.co>
2024-05-18 08:02:00 -0500

    0021 add styles summary ticket

--- src/app/modules/catalog/ticket/ticket.component.ts
+++ src/app/modules/catalog/ticket/ticket.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../../../shared/banner/banner.component';
 import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 @Component({
   selector: 'app-ticket',
   standalone: true,
   imports: [
     BannerComponent,
-    NzGridModule
+    NzGridModule,
+    NzButtonModule
   ],
   templateUrl: './ticket.component.html',
   styleUrl: './ticket.component.css'
