7d7b605e12ba8b0fff7b2206f0d6165616d3ffdb
edward vega <edward.vega@wiedii.co>
2024-05-17 22:49:39 -0500

    0019 refactor style banner and adjust routes

--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-banner',
   standalone: true,
@@ -14,5 +14,12 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
   styleUrl: './banner.component.css'
 })
 export class BannerComponent {
+
+  constructor(private router: Router) {}
+
   faArrowLeft = faArrowLeft
+
+  navigateToProducts() {
+    this.router.navigate(['/']);
+  }
 }
