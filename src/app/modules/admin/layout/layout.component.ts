fd39c3803d947444e1e9d3412a7d73cf1fd7c5c6
edward vega <edward.vega@wiedii.co>
2024-05-19 10:53:00 -0500

    0025 add styles section admin

--- src/app/modules/admin/layout/layout.component.ts
+++ src/app/modules/admin/layout/layout.component.ts
@@ -2,18 +2,27 @@ import { Component } from '@angular/core';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
-
+import { ProductsComponent } from '../products/products.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
     NzLayoutModule,
     NzBreadCrumbModule,
-    NzMenuModule
+    NzMenuModule,
+    ProductsComponent,
+    NzButtonModule
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
 
+  constructor(private router: Router) {}
+
+  goToProducts(): void {
+    this.router.navigate(['/']);
+  }
 }
--- src/app/modules/admin/products/products.component.ts
+++ src/app/modules/admin/products/products.component.ts
@@ -1,12 +1,191 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  ValidatorFn,
+  Validators
+} from '@angular/forms';
+import { Observable, Observer } from 'rxjs';
+
+import { NzSafeAny } from 'ng-zorro-antd/core/types';
+
+interface ItemData {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  availability: number;
+}
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [
+    NzTableModule,
+    NzInputModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
+    NzPopconfirmModule,
+    NzButtonModule,
+    NzModalModule
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
 
+export class ProductsComponent implements OnInit {
+
+  editCache: { [key: string]: { edit: boolean; data: ItemData } } = {};
+  listOfData: ItemData[] = [];
+  isVisible = false;
+  isFooter = null;
+
+  startEdit(id: string): void {
+    this.editCache[id].edit = true;
+  }
+
+  DeleteEdit(id: string): void {
+    const index = this.listOfData.findIndex(item => item.id === id);
+    this.editCache[id] = {
+      data: { ...this.listOfData[index] },
+      edit: false
+    };
+  }
+
+  cancelEdit(id: string): void {
+    const index = this.listOfData.findIndex(item => item.id === id);
+    this.editCache[id] = {
+      data: { ...this.listOfData[index] },
+      edit: false
+    };
+  }
+
+  saveEdit(id: string): void {
+    const index = this.listOfData.findIndex(item => item.id === id);
+    Object.assign(this.listOfData[index], this.editCache[id].data);
+    this.editCache[id].edit = false;
+  }
+
+  updateEditCache(): void {
+    this.listOfData.forEach(item => {
+      this.editCache[item.id] = {
+        edit: false,
+        data: { ...item }
+      };
+    });
+  }
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
+  }
+
+  ngOnInit(): void {
+    const data = [];
+    for (let i = 0; i < 100; i++) {
+      data.push({
+        id: `${i}`,
+        name: `Hacendado milk filled cereals ${i}`,
+        description: `Box 500g ${i}`,
+        price: 32,
+        availability:1
+      });
+    }
+    this.listOfData = data;
+    this.updateEditCache();
+  }
+
+  validateForm: FormGroup<{
+    nameNew: FormControl<string>;
+    descriptionNew: FormControl<string>;
+  }>;
+
+  // current locale is key of the nzAutoTips
+  // if it is not found, it will be searched again with `default`
+  autoTips: Record<string, Record<string, string>> = {
+    'zh-cn': {
+      required: 'Input is required'
+    },
+    en: {
+      required: 'Input is required'
+    }
+  };
+
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log('submit', this.validateForm.value);
+    } else {
+      Object.values(this.validateForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+
+  userNameAsyncValidator: AsyncValidatorFn = (control: AbstractControl) =>
+    new Observable((observer: Observer<MyValidationErrors | null>) => {
+      setTimeout(() => {
+        if (control.value === 'JasonWood') {
+          observer.next({
+            duplicated: { 'zh-cn': `The text is redundant!`, en: `The text is redundant!` }
+          });
+        } else {
+          observer.next(null);
+        }
+        observer.complete();
+      }, 1000);
+    });
+
+  constructor(private fb: NonNullableFormBuilder) {
+    // use `MyValidators`
+    const { required, maxLength, minLength } = MyValidators;
+    this.validateForm = this.fb.group({
+      nameNew: ['', [required, maxLength(12), minLength(6)], [this.userNameAsyncValidator]],
+      descriptionNew: ['', [required, maxLength(12), minLength(6)], [this.userNameAsyncValidator]],
+    });
+  }
+
+}
+// current locale is key of the MyErrorsOptions
+export type MyErrorsOptions = { 'zh-cn': string; en: string } & Record<string, NzSafeAny>;
+export type MyValidationErrors = Record<string, MyErrorsOptions>;
+
+export class MyValidators extends Validators {
+  static override minLength(minLength: number): ValidatorFn {
+    return (control: AbstractControl): MyValidationErrors | null => {
+      if (Validators.minLength(minLength)(control) === null) {
+        return null;
+      }
+      return { minlength: { 'zh-cn': `MinLength is ${minLength}`, en: `MinLength is ${minLength}` } };
+    };
+  }
+
+  static override maxLength(maxLength: number): ValidatorFn {
+    return (control: AbstractControl): MyValidationErrors | null => {
+      if (Validators.maxLength(maxLength)(control) === null) {
+        return null;
+      }
+      return { maxlength: { 'zh-cn': `MaxLength is ${maxLength}`, en: `MaxLength is ${maxLength}` } };
+    };
+  }
 }
