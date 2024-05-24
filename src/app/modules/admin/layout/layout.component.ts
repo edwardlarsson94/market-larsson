22335205fcf906f66310303687b03c1514ed3c1b
edward vega <edward.vega@wiedii.co>
2024-05-23 22:51:30 -0500

    0053 add clean user and cart in logout admin

--- src/app/modules/admin/layout/layout.component.ts
+++ src/app/modules/admin/layout/layout.component.ts
@@ -5,6 +5,10 @@ import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { ProductsComponent } from '../products/products.component';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../state/app.state';
+import { clearUser, clearCart } from '../../../state/app.actions';
+
 @Component({
   selector: 'app-layout',
   standalone: true,
@@ -20,9 +24,11 @@ import { Router } from '@angular/router';
 })
 export class LayoutComponent {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private store: Store<AppState>) {}
 
   goToProducts(): void {
+    this.store.dispatch(clearUser());
+    this.store.dispatch(clearCart());
     this.router.navigate(['/']);
   }
 }
